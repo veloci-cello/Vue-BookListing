34d70268b019814fb5c6af70b31b72f299051d4a
Jason McDermott <jmcdermott@rightpoint.com>
2018-10-20 15:32:54 -0500

    add BookItem

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -2,7 +2,7 @@
 <div>
     <h1>{{title}}</h1>
     <ul>
-        <li v-for="book in books">{{book.title}}: {{book.author}}</li>
+        <li v-for="book in books"></li>
     </ul>
 </div>
 </template>
@@ -25,15 +25,12 @@ export default {
 </script>
 
 <style>
-h1, h2 {
-    font-weight: normal
-}
-ul {
-    list-style-type: none;
-    padding: 0;
-}
-li {
-    display: block;
-    margin: 0 10px;
-}
+    h1, h2 {
+        font-weight: normal
+    }
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+
 </style>
